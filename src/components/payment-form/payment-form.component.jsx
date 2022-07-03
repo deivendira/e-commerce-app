7fb53f845a8098f044680d26f9864d90eea2aee6
Deivendira Brabu <deivendiran.isro@gmail.com>
2022-07-03 08:09:25 +0530

    money

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -26,7 +26,7 @@ const PaymentForm = () => {
     const response = await fetch("/.netlify/functions/create-payment-intent", {
       method: "post",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ amount: amount * 78.95 }),
+      body: JSON.stringify({ amount: amount * 100, currency: "INR" }),
     }).then((res) => {
       return res.json();
     });
